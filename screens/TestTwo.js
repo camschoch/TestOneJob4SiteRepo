f6e31c6ee5e04bace45efc4ca6405a4bb9b84805
camschoch <camschoch3@gmail.com>
2017-11-22 19:59:55 -0600

    working maps API

--- screens/TestTwo.js
+++ screens/TestTwo.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Dimensions, Text } from 'react-native';
+import { ScrollView, StyleSheet, Dimensions, Text, position, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+let { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 0;
+const LONGITUDE = 0;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class MapExample extends React.Component {
 
   static navigationOptions = {
-    title: 'TestTwo',
+    title: 'MapExample',
   };
   constructor() {
-  super();
-  this.state = {
-    region: {
-      latitude: 43.04,
-      longitude: -87.99,
-      latitudeDelta: 0.0043,
-      longitudeDelta:  0.0034,
-    }
-  };
-}
-
+    super();
+    this.state = {
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    };
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      },
+    (error) => console.log(error.message),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+    );
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
   render() {
     return (
       <MapView
@@ -27,7 +64,7 @@ export default class MapExample extends React.Component {
       showsUserLocation={ true }
       region={ this.state.region }
     >
-    </MapView>
+      </MapView>
     );
   }
 }
